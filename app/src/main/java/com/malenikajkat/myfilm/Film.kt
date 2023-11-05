88876e656fff21c206101bad677b0caef855d6f4
home <malenikajkat@mail.ru>
2023-11-05 14:58:55 +0300

    Modul_25.8

---
+++ app/src/main/java/com/malenikajkat/myfilm/Film.kt
@@ -0,0 +1,12 @@
+package com.malenikajkat.myfilm
+
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Film(
+    val title: String,
+    val poster: Int,
+    val description: String
+) : Parcelable
